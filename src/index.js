3efa376af1bb2b31202a26eeb6dd8606a95c0f9d
PiriCavanagh <82499709+PiriCavanagh@users.noreply.github.com>
2022-10-14 10:08:56 +1000

    djhdsfhjk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import { ReactDOM } from "react-dom";
-import '.index.css';
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+reportWebVitals();
