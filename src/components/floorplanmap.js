6aa5c39362c743c51d996aa9cae1e2dc44e2e89e
PiriCavanagh <82499709+PiriCavanagh@users.noreply.github.com>
2022-10-14 11:25:54 +1000

    dfjhsdfhj

--- src/components/floorplanmap.js
+++ src/components/floorplanmap.js
@@ -2,27 +2,27 @@
 const floorplanmap = () => {
   return (
     <div class="map">
-      <div class="room101"><a href="#">101</a></div>
-      <div class="room102"><a href="#">102</a></div>
-      <div class="room103"><a href="#">103</a></div>
-      <div class="room104"><a href="#">104</a></div>
-      <div class="room105"><a href="#">105</a></div>
-      <div class="room106"><a href="#">106</a></div>
-      <div class="room107"><a href="#">107</a></div>
-      <div class="room108"><a href="#">108</a></div>
-      <div class="room109"><a href="#">109</a></div>
-      <div class="room110"><a href="#">110</a></div>
-      <div class="room111"><a href="#">111</a></div>
-      <div class="room112"><a href="#">112</a></div>
-      <div class="room113"><a href="#">113</a></div>
-      <div class="room114"><a href="#">114</a></div>
-      <div class="room115"><a href="#">115</a></div>
-      <div class="room116"><a href="#">116</a></div>
-      <div class="room117"><a href="#">117</a></div>
-      <div class="room118"><a href="#">118</a></div>
-      <div class="room119"><a href="#">119</a></div>
-      <div class="room120"><a href="#">120</a></div>
-      <div class="room121"><a href="#">120</a></div>
+      <div class="room101"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">101</button></div>
+      <div class="room102"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">102</button></div>
+      <div class="room103"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">103</button></div>
+      <div class="room104"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">104</button></div>
+      <div class="room105"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">105</button></div>
+      <div class="room106"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">106</button></div>
+      <div class="room107"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">107</button></div>
+      <div class="room108"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">108</button></div>
+      <div class="room109"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">109</button></div>
+      <div class="room110"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">110</button></div>
+      <div class="room111"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">111</button></div>
+      <div class="room112"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">112</button></div>
+      <div class="room113"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">113</button></div>
+      <div class="room114"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">114</button></div>
+      <div class="room115"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">115</button></div>
+      <div class="room116"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">116</button></div>
+      <div class="room117"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">117</button></div>
+      <div class="room118"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">118</button></div>
+      <div class="room119"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">119</button></div>
+      <div class="room120"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">120</button></div>
+      <div class="room121"><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">121</button></div>
       <div class="whitespace1"></div>
       <div class="whitespace2"></div>
       <div class="whitespace3"></div>
