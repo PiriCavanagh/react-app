f7abce8d49f83a7e0e1d5c22ea268a32664aa8ad
PiriCavanagh <82499709+PiriCavanagh@users.noreply.github.com>
2022-10-20 17:26:20 +1000

    move improvements

--- src/second_page_components/Roommap.js
+++ src/second_page_components/Roommap.js
@@ -1,7 +1,15 @@
+const ball = {
+    position: 'absolute',
+    top: '20',
+    left: '20',
+    width: '50px',
+    height: '50px',
+    background: 'red',
+};
 const Roommap = () => {
     return (
         <div className='roommap'>
-
+            <div style={ball}></div>
         </div>
     );
   }
