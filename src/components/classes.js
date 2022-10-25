72384e7eb1b391b3f2b60247836f7218a5c63568
PiriCavanagh <82499709+PiriCavanagh@users.noreply.github.com>
2022-10-25 14:29:26 +1000

    dmkfsdklj

---
+++ src/components/classes.js
@@ -0,0 +1,18 @@
+// function person(fullname, roll,doctername) {
+//     this.fullname = fullname
+//     this.roll = roll
+//     this.doctername = doctername
+// }
+// function consulation(starttime,endtime,patientname,doctername) {
+//     this.starttime = starttime
+//     this.endtime = endtime
+//     this.patientname = patientname
+//     this.doctername = doctername
+// }
+// function room(roomnid,usestatus, Occupants[] ,Consultations[]) {
+//     this.roomnid = roomnid
+//     this.usestatus = usestatus
+//     this.Occupants[] = Occupants[]
+//     this.Consultations[] =Consultations[]
+// }
+  
--- src/second_page_components/Roommap.js
+++ src/second_page_components/Roommap.js
@@ -1,15 +1,27 @@
 const ball = {
     position: 'absolute',
+    borderRadius: '20px',
     top: '20',
     left: '20',
-    width: '50px',
-    height: '50px',
+    width: '30px',
+    height: '30px',
+    background: 'rgba(255, 0, 0, .5)',
+    boxShadow: '2px 2px 10px rgba(255, 0, 0, .5)',
+    display:'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+};
+const innerball = {
+    borderRadius: '20px',
+    width: '20px',
+    height: '20px',
     background: 'red',
+
 };
 const Roommap = () => {
     return (
         <div className='roommap'>
-            <div style={ball}></div>
+            <div style={ball}><div style={innerball}></div></div>
         </div>
     );
 }
