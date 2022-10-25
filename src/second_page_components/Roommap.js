b2cef1b784313f777ce1f0a97284d29593503246
Oli <oliivett2001@gmail.com>
2022-10-25 18:00:54 +1000

    yo

--- src/second_page_components/Roommap.js
+++ src/second_page_components/Roommap.js
@@ -1,16 +1,7 @@
-const ball = {
-    position: 'absolute',
-    borderRadius: '20px',
-    top: '20',
-    left: '20',
-    width: '30px',
-    height: '30px',
-    background: 'rgba(255, 0, 0, .5)',
-    boxShadow: '2px 2px 10px rgba(255, 0, 0, .5)',
-    display:'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-};
+import{useState,useEffect} from "react";
+
+
+
 const innerball = {
     borderRadius: '20px',
     width: '20px',
@@ -18,12 +9,50 @@ const innerball = {
     background: 'red',
 
 };
-const Roommap = () => {
+function Roommap() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("http://54.87.72.37:5000/api/getCoordinates")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              setIsLoaded(true);
+              setItems(result);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+              }
+            )
+        }, [])
+
+        if (error) {
+            return <div>Error: {error.message}</div>;
+          } else if (!isLoaded) {
+            return <div>Loading...</div>;
+          } else {
     return (
+        
         <div className='roommap'>
-            <div style={ball}><div style={innerball}></div></div>
+            
+            {items.map(item => ( 
+            <div  style={{position: 'absolute',
+            borderRadius: '20px',
+            transform: `translate(${item.x}px, ${item.y}px)`,
+            width: '30px',
+            height: '30px',
+            background: 'rgba(255, 0, 0, .5)',
+            boxShadow: '2px 2px 10px rgba(255, 0, 0, .5)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',}}><div style={innerball}></div></div>
+            ))}
         </div>
     );
+}
 }
 
 export default Roommap
