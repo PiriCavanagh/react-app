0b22a0519d28d46a025bd6ae118636adb583c091
Oli <oliivett2001@gmail.com>
2022-10-25 19:53:05 +1000

    fdwsa

--- src/components/floorplanmap.js
+++ src/components/floorplanmap.js
@@ -1,4 +1,3 @@
-import { BrowserRouter, Route, Link } from "react-router-dom";
 import '../index.css'
 import Roomcomponent from "./roomcomponent";
 
@@ -61,23 +60,6 @@ const floorplanmap = () => {
     </div>
 
   );
-}
-
-const exampleModal = document.getElementById('exampleModal')
-if (exampleModal) {
-  exampleModal.addEventListener('show.bs.modal', event => {
-    // Button that triggered the modal
-    const button = event.relatedTarget
-    // Extract info from data-bs-* attributes
-    const recipient = button.getAttribute('data-bs-whatever')
-
-    // Update the modal's content.
-    const modalTitle = exampleModal.querySelector('.modal-title')
-    const modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-    modalTitle.textContent = `New message to ${recipient}`
-    modalBodyInput.value = recipient
-  })
 }
 
 
