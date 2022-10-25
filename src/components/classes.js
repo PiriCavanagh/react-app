1be516230b5afa82b5575815fe8d8fa46874122b
PiriCavanagh <82499709+PiriCavanagh@users.noreply.github.com>
2022-10-25 15:34:15 +1000

    dsafjkdfsakj

--- src/components/classes.js
+++ src/components/classes.js
@@ -1,18 +1,18 @@
-import{usestate} from "react";
-function person(props) {
-    const [fullname, setfullname] = useState("");
-    const [roll, setroll] = useState("");
-    const [doctername, setdoctername] = useState("");
+import{useState} from "react";
+function person(...props) {
+    const [fullname, setFullname] = useState("");
+    const [roll, setRoll] = useState("");
+    const [doctername, setDoctername] = useState("");
 }
 function consulation(props) {
-    const [starttime, setstarttime] = useState("");
-    const [endtime, setendtime] = useState("");
-    const [patientname, setpatientname] = useState("");
-    const [doctername, setdoctername] = useState("");
+    const [starttime, setStarttime] = useState("");
+    const [endtime, setEndtime] = useState("");
+    const [patientname, setPatientname] = useState("");
+    const [doctername, setDoctername] = useState("");
 }
 function room(props) {
-    const [roomid, setroomid] = useState(null);
-    const [usestatus, setusestatus] = useState(false);
+    const [roomid, setRoomid] = useState(null);
+    const [usestatus, setUsestatus] = useState(false);
     const [Occupants, setOccupants] = useState([]);
     const [Consultations, setConsultations] = useState([]);
 }
